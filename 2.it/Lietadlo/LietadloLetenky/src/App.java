5bb572c3afb27de18efa314a173680fe527a27b3
Experrrt <jakubmarcek955@gmail.com>
2021-01-21 20:09:33 +0100

    commit

---
+++ 2.it/Lietadlo/LietadloLetenky/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        LietadloLetenky lietadlo = new LietadloLetenky("VIENNA", "NEW YORK", 7, 4900, 380);
+        lietadlo.mainLoop();
+    }
+}
---
+++ 2.it/Lietadlo/LietadloLetenky/src/LietadloLetenky.java
@@ -0,0 +1,126 @@
+import java.util.*;
+import java.util.Random;
+import org.nocrala.tools.texttablefmt.BorderStyle;
+import org.nocrala.tools.texttablefmt.Table;
+
+public class LietadloLetenky {
+    int pocRadov = 6, pocSedadiel, cenaPrvaT, cenaDruhaT, obPrvaT, obDruhaT;
+    String odkial, kam;
+    String listSedadla[][];
+
+    LietadloLetenky(String odkial, String kam, int pocetSedadiel, int cenaPrvaT, int cenaDruhaT) {
+        this.odkial = odkial;
+        this.kam = kam;
+        this.pocSedadiel = pocetSedadiel;
+        this.cenaPrvaT = cenaPrvaT;
+        this.cenaDruhaT = cenaDruhaT;
+        listSedadla = new String[pocRadov][pocetSedadiel];
+    }
+
+    public void mainLoop() {
+        generujLietadlo();
+        Scanner sc = new Scanner(System.in);
+        int odpoved = -1;
+        while (odpoved != 0) {
+            System.out.println("[" + odkial + " - " + kam + "]\n");
+            System.out.println("1. Obsadenosť");
+            System.out.println("2. Predať letenku");
+            System.out.println("3. Štatistika");
+            System.out.println("0. Ukončiť \n");
+
+            System.out.print("Voľba: ");
+            odpoved = sc.nextInt();
+            System.out.println();
+            switch (odpoved) {
+                case 1:
+                    vykresliLietadlo();
+                    break;
+                case 2:
+                    System.out.println("Predať letenku");
+                    break;
+                case 3:
+                    vypisStatistiku();
+                    break;
+            }
+            System.out.println();
+        }
+    }
+
+    public void generujLietadlo() {
+        Random random = new Random();
+        for (int i = 0; i < pocRadov; i++) {
+            int altCode = 64;
+            for (int y = 0; y < pocSedadiel; y++) {
+                altCode++;
+                if (random.nextInt(100) < 20) {
+                    listSedadla[i][y] = "\u001B[31m" + Integer.toString(i + 1) + (char) altCode + "\u001B[0m";
+                    if (i == 0)
+                        obPrvaT++;
+                    else
+                        obDruhaT++;
+
+                    continue;
+                }
+                listSedadla[i][y] = Integer.toString(i + 1) + (char) altCode;
+            }
+        }
+    }
+
+    public void vypisStatistiku() {
+        int celkovyPocetS = pocRadov * pocSedadiel;
+        float pocObsaden = obPrvaT + obDruhaT;
+        int moznaSuma = pocSedadiel * cenaPrvaT + (celkovyPocetS - pocSedadiel) * cenaDruhaT;
+        float zarobenaSuma = obPrvaT * cenaPrvaT + obDruhaT * cenaDruhaT;
+
+        System.out.println("Počet miest: " + celkovyPocetS);
+        System.out.printf("Počet obsadených miest: %.0f (%.1f%%) %n", pocObsaden, (pocObsaden / celkovyPocetS) * 100);
+        System.out.printf("Možné výnosy: %,d %n", moznaSuma);
+        System.out.printf("Suma za predané lístky: %,.0f (%.1f%%) %n", zarobenaSuma, (zarobenaSuma / moznaSuma) * 100);
+    }
+
+    public void vykresliLietadlo() {
+        if (pocSedadiel < 4 || pocSedadiel > 9 || pocRadov < 4 || pocRadov > 9) {
+            System.out.println("Nespravne zadané parametre");
+            System.exit(0);
+        }
+
+        ArrayList<Integer> ulicky = new ArrayList<Integer>();
+
+        switch (pocSedadiel) {
+            case 6:
+                ulicky.add(3);
+                break;
+            case 7:
+                ulicky.add(2);
+                ulicky.add(6);
+                break;
+            case 8:
+                ulicky.add(2);
+                ulicky.add(7);
+                break;
+            case 9:
+                ulicky.add(3);
+                ulicky.add(7);
+                break;
+            case 4:
+            case 5:
+                ulicky.add(2);
+                break;
+        }
+        Table table = new Table(pocSedadiel + ulicky.size(), BorderStyle.DESIGN_FORMAL_WIDE);
+
+        for (int z = 0; z < pocRadov; z++) {
+            int pouziteUlicky = 0;
+            for (int i = 0; i < pocSedadiel + ulicky.size(); i++) {
+                if (ulicky.contains(i)) {
+                    pouziteUlicky++;
+                    table.addCell(null);
+                } else {
+                    table.addCell(listSedadla[z][i - pouziteUlicky]);
+                }
+            }
+        }
+        System.out.println(table.render() + "\n");
+        System.out.println("Počet obsadených miest: " + (obDruhaT + obPrvaT));
+    }
+}
